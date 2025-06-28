6e3b34db20f47251982d141fda74f4b6be6e2543
Sandhya-Subbiah <sandhya.subbiah07@gmail.com>
2025-06-28 14:50:54 +0530

    Renamed project folder and fixed settings imports

--- InternSync/wsgi.py
+++ InternSync/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InternSync.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internsync.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InternSync.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internsync.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
